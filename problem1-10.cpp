4b2c8174db069230951aa81d74390e7105992480
smasr07 <a.sobirjonov@newuu.uz>
2024-09-18 19:25:05 +0500

    Update and rename lab to problem1-10.cpp

---
+++ problem1-10.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+using namespace std;
+int main() {
+      /*int a;
+      cin >> a;
+      if ( a >= 0) cout << "Positive " << endl;
+      else cout << "Negative " << endl;
+      */
+      /* int a;
+       cin >> a;
+       if ( a >= 0 && a%2 == 0 ) cout << "The number is positive and even " << endl;
+       else if ( a>= 0 && a%2 == 1 ) cout << "The number is positive and odd " << endl;
+       else if ( a < 0 && a%2 == 0 )  cout << "The number is negative and even " << endl;
+       else cout << "The number is negative and odd " << endl;
+       return 0;*/
+      /*int a, b, c;
+      cin >> a >> b >> c;
+      if ( a > b && a > c ) { cout << a << " is the largest " << endl; }
+      if ( b > a && b > c ) { cout << b << " is the largest " << endl; }
+      if ( c > b && c > a ) { cout << c << " is the largest " << endl; }
+  */
+      /*int a, b;
+      cin >> a >> b;
+      if ( a % b == 0 ) cout << a << " is divisible by " << b << endl;
+      else cout << a << " is not divisible by " << b << endl;*/
+      /*int a;
+      cin >> a;
+      if ( (a%100 == 0  && a%400 != 0) || a % 4 != 0 ) { cout << a << " is not a leap year " << endl; }
+      else  { cout << a << " is  a leap year" << endl; }*/
+      /* float a, b, c, x1, x2;
+       cin >> a >> b >> c;
+       x1 = ( -b + sqrt(( (b * b) - (4 * a * c )) ) ) / (2 * a);
+       x2 = ( -b - sqrt(( (b * b) - (4 * a * c )) ) ) / (2 * a);
+       cout << " x1 = " << x1 << " " << " x2 = " << x2 << endl;*/
+      /*int a, b, c;
+      cin >> a >> b >> c;
+      if ( a + b + c == 180) cout << " The triangle is valid " << endl;
+      else cout << " The triangle is invalid " << endl;*/
+      /*char a;
+      cin >> a;
+      int a_char= a;
+      if ( a_char >=65 && a_char<=90 ) { cout << "Uppercase alphabet " << endl;}
+      if ( a_char >= 97 && a_char <= 122 ) { cout << "Lowercase alphabet " << endl;}
+      else {cout << "It is not an alphabet " << endl;}*/
+      /*int a, b;
+      cin >> a >> b;
+      if ( pow((pow(a,2)+pow(b,2)) , 0.5) <= 10) cout << " Point ( " << a << ", " << b << " ) is in circle" << endl;
+      else cout << " Point ( " << a << ", " << b << " ) is not in circle" << endl;*/
+      /*float weight1, price1, weight2, price2;
+      cout << " Enter weight and price for package 1" << endl;
+      cin >> weight1 >> price1;
+      cout << " Enter weight and price for package 2";
+      cin >> weight2 >> price2;
+      if ( price1/weight1 > price2/weight2 ) cout << " Package 2 has a better price" << endl;
+      else if ( price1/weight1 == price2/weight2 ) cout << " Two packages have the same price " << endl;
+      else cout << " Package 1 has a better price" << endl;*/
+
+
+
+
+
+
+    return 0;
+}
